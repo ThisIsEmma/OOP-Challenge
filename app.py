637e933325007501f9f1bcb7607f6a579b6f47cc
ThisIsEmma <sakatia.lise@gmail.com>
2022-11-28 17:54:44 -0500

    some brainstorming

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-# 4 Classes
+#Program needs 4 Classes
+
+# Each class need 2 methods (modify the class a) 
+
 # 1 class for main player
 # 1 class for food
 # 1 class for bomb
 # 1 class for surface 
 
 
-# Inheritance
-# PROPS - Food + Bomb 
+# 1 - Inheritance
+# PROPS (parent class) = Food + Bomb 
+'''
+PROPS: 
+attribute:
+    name
+
+    appear() - populate item on the surface
+    disapear() - remove item from the surface
+
+'''
+
+'''
+SNAKE:
+    initial-length
+
+    up()
+    down()
+    left()
+    right()
+'''
